74f09d24b11b7e1a8bbb8557f11d3988323aa69b
rmsgh1202 <54826629+rmsgh1202@users.noreply.github.com>
2019-10-15 12:17:54 +0900

    Add files via upload

--- resource.h
+++ resource.h
@@ -1,21 +1,24 @@
 ﻿//{{NO_DEPENDENCIES}}
 // Microsoft Visual C++에서 생성한 포함 파일입니다.
-// img.rc에서 사용되고 있습니다.
+// md2.rc에서 사용되고 있습니다.
 //
-#define IDM_ABOUTBOX                    0x0010
 #define IDD_ABOUTBOX                    100
-#define IDS_ABOUTBOX                    101
-#define IDD_IMG_DIALOG                  102
+#define IDP_OLE_INIT_FAILED             100
 #define IDR_MAINFRAME                   128
-#define IDC_BUTTON1                     1000
+#define IDR_md2TYPE                     130
+#define ID_MENU_COL                     32771
+#define ID_MENU_SIZE                    32772
+#define ID_SIZE_1                       32773
+#define ID_SIZE_16                      32774
+#define ID_SIZE_32                      32775
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        130
-#define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1001
-#define _APS_NEXT_SYMED_VALUE           101
+#define _APS_NEXT_RESOURCE_VALUE        310
+#define _APS_NEXT_COMMAND_VALUE         32776
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           310
 #endif
 #endif
